0cb269bb51703f6a20749989b6040bd7192ee80a
mosquito242 <anshul.jain242@gmail.com>
2012-12-28 15:41:13 -0800

    transitioning from AVAudioPlayer to audio sessions.

--- RectorScale/RectorScaleMainViewController.h
+++ RectorScale/RectorScaleMainViewController.h
@@ -7,8 +7,17 @@
 //
 
 #import "RectorScaleFlipsideViewController.h"
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface RectorScaleMainViewController : UIViewController <RectorScaleFlipsideViewControllerDelegate>
+@interface RectorScaleMainViewController : UIViewController <RectorScaleFlipsideViewControllerDelegate, AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+@property (strong, nonatomic) AVAudioRecorder *audioRecorder;
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
+@property BOOL didInitialiseSession;
+@property (weak, nonatomic) IBOutlet UIButton *recordButton;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+- (IBAction)recordAudio:(id)sender;
+- (IBAction)playAudio:(id)sender;
 
 - (IBAction)showInfo:(id)sender;
 
--- RectorScale/RectorScaleMainViewController.m
+++ RectorScale/RectorScaleMainViewController.m
@@ -18,6 +18,43 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _didInitialiseSession = NO;
+    _playButton.enabled = NO;
+    
+    NSArray *dirPaths;
+    NSString *docsDir;
+    
+    dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask,YES);
+    docsDir = dirPaths[0];
+    
+    NSString *soundFilePath = [docsDir stringByAppendingPathComponent:@"sound.caf"];
+    
+    NSURL *soundFileURL = [NSURL fileURLWithPath:soundFilePath];
+    
+    
+    NSDictionary *recordSettings = [NSDictionary
+                                    dictionaryWithObjectsAndKeys:
+                                    [NSNumber numberWithInt:AVAudioQualityMin],
+                                    AVEncoderAudioQualityKey,
+                                    [NSNumber numberWithInt:16],
+                                    AVEncoderBitRateKey,
+                                    [NSNumber numberWithInt: 2],
+                                    AVNumberOfChannelsKey,
+                                    [NSNumber numberWithFloat:44100.0],
+                                    AVSampleRateKey,
+                                    nil];
+    
+    NSError *error = nil;
+    
+    _audioRecorder = [[AVAudioRecorder alloc]
+                     initWithURL:soundFileURL
+                     settings:recordSettings
+                     error:&error];
+    
+    if(error) {
+        NSLog(@"error: %@", [error localizedDescription]);
+    }
+    else { [_audioRecorder prepareToRecord]; }
 }
 
 - (void)didReceiveMemoryWarning
@@ -33,12 +70,57 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (IBAction)recordAudio:(id)sender {
+    if(!_didInitialiseSession) {
+    
+        AudioSessionInitialize(NULL, NULL, NULL, NULL);
+        _didInitialiseSession= YES;
+    }
+    
+    OSStatus activationResult = NULL;
+    activationResult = AudioSessionSetActive (true);
+        
+      
+}
+
+- (IBAction)playAudio:(id)sender {
+       
+}
+
+
 - (IBAction)showInfo:(id)sender
 {    
     RectorScaleFlipsideViewController *controller = [[RectorScaleFlipsideViewController alloc] initWithNibName:@"RectorScaleFlipsideViewController" bundle:nil];
     controller.delegate = self;
     controller.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
     [self presentViewController:controller animated:YES completion:nil];
+}
+
+-(void)audioPlayerDidFinishPlaying:
+(AVAudioPlayer *)player successfully:(BOOL)flag
+{
+    _recordButton.enabled = YES;
+    
+}
+
+-(void)audioPlayerDecodeErrorDidOccur:
+(AVAudioPlayer *)player
+                                error:(NSError *)error
+{
+    NSLog(@"Decode Error occurred");
+}
+
+-(void)audioRecorderDidFinishRecording:
+(AVAudioRecorder *)recorder
+                          successfully:(BOOL)flag
+{
+}
+
+-(void)audioRecorderEncodeErrorDidOccur:
+(AVAudioRecorder *)recorder
+                                  error:(NSError *)error
+{
+    NSLog(@"Encode Error occurred");
 }
 
 @end
